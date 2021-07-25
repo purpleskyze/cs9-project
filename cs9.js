d45d7839a0250351447a96182bf6539993ff768b
purpleskyze <65695664+purpleskyze@users.noreply.github.com>
2021-07-25 14:33:19 -0400

    Add files via upload

---
+++ cs9.js
@@ -0,0 +1,35 @@
+
+function setup() {
+  createCanvas(400,400);
+
+  red_btn=createButton("RED");
+red_btn.position(100,50);
+red_btn.mousePressed=(red_bg);
+
+green_btn=createButton("GREEN");
+green_btn.positon(200,50);
+green_btn.mousePressed=(green_bg)
+}
+
+function draw() 
+{
+  background(30);
+
+  function red_bg()
+
+r=255;
+g=0;
+b=0;
+
+function green_bg()
+
+r=0;
+g=255;
+b=0;
+
+drawsSprites()
+}
+
+
+
+
